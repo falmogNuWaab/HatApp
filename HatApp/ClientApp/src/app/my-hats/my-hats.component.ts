bbc50cd4915608c3a726eadfbd7b4734e4776769
James Mitchell <jfalmogusa@gmail.com>
2022-02-09 09:01:02 -0500

    Pushing up wear hat work

--- HatApp/ClientApp/src/app/my-hats/my-hats.component.ts
+++ HatApp/ClientApp/src/app/my-hats/my-hats.component.ts
@@ -4,7 +4,8 @@ import {UserObject, UOConvert} from '../UserObject';
 import { UserService } from '../user.service';
 import { Hat, HatConvert } from '../Hat';
 import { HatService } from '../hat.service';
-
+import {MonthlyHats} from '../Monthly-Hats';
+import { Inject } from '@angular/core';
 
 @Component({
   selector: 'app-my-hats',
@@ -16,7 +17,8 @@ export class MyHatsComponent implements OnInit {
 currentUser:UserObject;
 allHats: Hat[] = [];
 id = Number(this.route.snapshot.paramMap.get('id'));
-  constructor(private userService: UserService, private route: ActivatedRoute, private hatService: HatService) {
+url: string;
+  constructor(private userService: UserService, private route: ActivatedRoute, private hatService: HatService, @Inject('BASE_URL')baseUrl: string) {
   this.userService.GetUserObject(this.id).subscribe(
     (response: any)=>{
       let json = UOConvert.userObjectToJson(response);
@@ -29,7 +31,8 @@ id = Number(this.route.snapshot.paramMap.get('id'));
         let json= HatConvert.hatToJson(response);
         this.allHats = HatConvert.toHat(json);
       } 
-    );   
+    );
+    this.url = baseUrl + "ShowUsers/MyHats/" + this.id;   
 }
 
 ngOnInit() {
@@ -39,5 +42,20 @@ ngOnInit() {
 RemoveFromCollection(id: number) {
   this.hatService.DeleteMyHat(id);
 }
-
+WearHat(hID: number){
+  let today = new Date();
+  let mm = today.getDate().toString().padStart(2,"0");
+  let dd = today.getDay().toString().padStart(2,"0");
+  let yyyy = today.getFullYear().toString();  
+  let lwd: string = mm + "/" + dd + "/" + yyyy;
+  let tempHat: MonthlyHats = {id:0,userId:this.id,hatId:hID,lastWornDate:lwd};
+  console.log("Uid: " + this.id);
+  console.log("Hid: " + hID);
+  console.log("lwd: "+ lwd);
+  console.log(tempHat);
+  this.hatService.WearThisHat(tempHat).subscribe(
+    (response: any)=>{    
+    }
+  );
+}
 }
